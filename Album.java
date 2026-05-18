8cbc6c948f8faef82a4f4128727c4320b757c0b5
Hamid <hamidheydarian86@gmail.com>
2026-05-18 22:13:25 +0330

    Photo.java and Album.java updated

--- Album.java
+++ Album.java
@@ -1,3 +1,30 @@
+import java.util.List;
+
 public class Album {
+    private User owner;
+    private List<Photo> photos;
+    public Album(User owner, List<Photo> photos){
+        this.owner = owner;
+        this.photos = photos;
+    }
     
+    /* getter setter begin */
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+    public User getOwner() {
+        return owner;
+    }
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+    /* getter setter end */
+
+    public int totalLikes(){
+        // TODO
+        return 0;
+    }
 }
--- Photo.java
+++ Photo.java
@@ -1,3 +1,40 @@
+import java.util.List;
+
 public class Photo {
-    
+    private User owner;
+    private List<User> likedBy;
+    private long id;
+    private String path; // ?
+    public Photo(User owner, String path){
+        this.owner = owner;
+        this.path = path; // ?
+        this.id = 0; // TODO
+    }
+
+    /* getter setter begin */
+    public List<User> getLikedBy() {
+        return likedBy;
+    }
+    public void setLikedBy(List<User> likedBy) {
+        this.likedBy = likedBy;
+    }
+    public User getOwner() {
+        return owner;
+    }
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getPath() {
+        return path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+    /* getter setter end */
 }
