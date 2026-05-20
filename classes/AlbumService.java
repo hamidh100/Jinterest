95d822f204b43f2ff7de12ae4d335d0da8e3cc09
Hamid <hamidheydarian86@gmail.com>
2026-05-20 14:48:33 +0330

    static is better

--- classes/AlbumService.java
+++ classes/AlbumService.java
@@ -1,5 +1,5 @@
 public class AlbumService {
-    public int totalLikes(Album album){
+    public static int totalLikes(Album album){
         int totalLikes = 0;
         for (Photo photo : album.getPhotos()){
             totalLikes += (photo.getLikedBy() == null ? 0 : photo.getLikedBy().size());
--- classes/PhotoService.java
+++ classes/PhotoService.java
@@ -1,16 +1,16 @@
 public class PhotoService {
-    public boolean isLikedBy(Photo photo, User user){
+    public static boolean isLikedBy(Photo photo, User user){
         if (photo.getLikedBy() == null || photo.getLikedBy().size() == 0) return false;
         for (User someone : photo.getLikedBy()){
             if (someone != null && someone.equals(user)) return true;
         }
         return false;
     }
-    public void addLike(Photo photo, User user){
+    public static void addLike(Photo photo, User user){
         if (isLikedBy(photo, user)) return;
         photo.getLikedBy().add(user);
     }
-    public void removeLike(Photo photo, User user){
+    public static void removeLike(Photo photo, User user){
         if (!isLikedBy(photo, user)) return;
         photo.getLikedBy().remove(user);
     }
