eb8684d2fee85c89f8dba16755b30e7f5186a1f3
Hamid <hamidheydarian86@gmail.com>
2026-05-20 14:56:17 +0330

    oops

--- classes/exceptions/WeakPassword.java
+++ classes/exceptions/WeakPassword.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class WeakPassword extends Exception {
     public WeakPassword(){
-        super("\"Password must be at least 8 characters and include uppercase, lowercase and digits\"");
+        super("Password must be at least 8 characters and include uppercase, lowercase and digits");
     }
 }
