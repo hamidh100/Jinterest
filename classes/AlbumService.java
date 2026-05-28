ed595bc87428350fe102702febe7b36841bcad68
Hamid <hamidheydarian86@gmail.com>
2026-05-28 21:38:00 +0330

    saving UUIDs instead of objects

--- classes/AlbumService.java
+++ classes/AlbumService.java
@@ -1,13 +1,16 @@
+import java.util.UUID;
+
 public class AlbumService {
     public static int totalLikes(Album album){
         int totalLikes = 0;
-        for (Photo photo : album.getPhotos()){
+        for (UUID photoID : album.getPhotoIDs()){
+            Photo photo = OurObjects.photos.get(photoID);
             totalLikes += (photo.getLikedBy() == null ? 0 : photo.getLikedBy().size());
         }
         return totalLikes;
     }
     public static void addAlbum(User user, Album album) {
-        album.setOwner(user);
+        album.setOwnerID(user.getUuid());
         user.getAlbumIDs().add(album.getUuid());
     }
 }
