943837963091e0830ce3da027d752688b9eed3f7
Hamid <hamidheydarian86@gmail.com>
2026-05-28 21:34:00 +0330

    saving UUIDs instead of objects

--- classes/Like.java
+++ classes/Like.java
@@ -3,31 +3,31 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class Like {
-    private User user;
+    private UUID userID;
     private LocalDateTime time; // not visible publicly, but admins can see
     private final UUID uuid;
 
-    public Like(User user){
-        this.user = user;
+    public Like(UUID userID){
+        this.userID = userID;
         time = LocalDateTime.now(); // ?
         uuid = UUID.randomUUID();
         OurObjects.likes.put(uuid, this);
     }
 
-    public Like(User user, LocalDateTime time){
-        this.user = user;
+    public Like(UUID userID, LocalDateTime time){
+        this.userID = userID;
         this.time = time;
         uuid = UUID.randomUUID();
         OurObjects.likes.put(uuid, this);
     }
 
     /* getter setter begin */
-    public User getUser(){
-        return user;
+    public UUID getUserID() {
+        return userID;
     }
 
-    public void setUser(User user){
-        this.user = user;
+    public void setUserID(UUID userID) {
+        this.userID = userID;
     }
 
     public LocalDateTime getTime(){
