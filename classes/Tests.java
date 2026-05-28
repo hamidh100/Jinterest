8a8ab274c3c878f2b727f6b7c8ef33990622d285
Hamid <hamidheydarian86@gmail.com>
2026-05-28 21:25:09 +0330

    Tests initialized

---
+++ classes/Tests.java
@@ -0,0 +1,26 @@
+import org.junit.*;
+
+public class Tests {
+    @Before
+    public void init (){}
+
+    @Test
+    public void userSignUpAndLogin(){
+
+    }
+
+    @Test
+    public void addPhoto(){
+
+    }
+
+    @Test
+    public void addAlbum(){
+
+    }
+
+    @Test
+    public void addAndRemoveLike(){
+        
+    }
+}
