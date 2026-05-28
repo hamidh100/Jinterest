45e4abf9eee4658392dab4bb1b3e442c33fba9ee
Hamid <hamidheydarian86@gmail.com>
2026-05-28 17:13:02 +0330

    first commit in this branch : adding map from uuids to objs

---
+++ classes/Objects.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+import java.util.UUID;
+
+public class Objects {
+    public Map<UUID, User> users;
+    public Map<UUID, Photo> photos;
+    public Map<UUID, Album> albums;
+    public Map<UUID, Like> likes;
+    public Map<UUID, Comment> comments;
+}
