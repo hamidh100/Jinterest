ca7d34de9f0fd8281336c861b6519ffbcfab235d
Hamid <hamidheydarian86@gmail.com>
2026-05-30 20:35:06 +0330

    it doesn't need any exceptions

--- classes/User.java
+++ classes/User.java
@@ -4,9 +4,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-import exceptions.InvalidUsername;
-import exceptions.WeakPassword;
-
 public class User {
     private String phone, email, username, password, fullname;
     private final UUID uuid;
