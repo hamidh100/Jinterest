a57f42474a8c45394a0953e3f3b8d823425ef866
Hamid <hamidheydarian86@gmail.com>
2026-06-05 20:04:13 +0330

    comment initialized

--- classes/Comment.java
+++ classes/Comment.java
@@ -1,3 +1,9 @@
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Comment {
-    
+    private UUID photoID;
+    private UUID userID;
+    private String text;
+    private LocalDateTime time;
 }
