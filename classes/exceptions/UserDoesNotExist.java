6b823f331a0cc8ddbe5abadf2c129f3e36b3c231
Hamid <hamidheydarian86@gmail.com>
2026-06-05 17:10:47 +0330

    nothing.

--- classes/exceptions/UserDoesNotExist.java
+++ classes/exceptions/UserDoesNotExist.java
@@ -1,7 +1,6 @@
 package exceptions;
 
 public class UserDoesNotExist extends Exception {
-
     public UserDoesNotExist(String identifier) {
         super("User does not exist: " + identifier);
     }
