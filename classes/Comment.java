1f0a7b155bce1c82d389d35d451479faf65d72e6
Hamid <hamidheydarian86@gmail.com>
2026-06-07 21:44:14 +0330

    comment improved

--- classes/Comment.java
+++ classes/Comment.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Comment {
@@ -6,4 +7,62 @@ public class Comment {
     private UUID userID;
     private String text;
     private LocalDateTime time;
+    private final UUID uuid;
+
+    public Comment(UUID photoID, UUID userID, String text) {
+        this.photoID = photoID;
+        this.userID = userID;
+        this.text = text;
+        time = LocalDateTime.now();
+        uuid = UUID.randomUUID();
+    }
+
+    /* getter setter begin */
+    public UUID getPhotoID() {
+        return photoID;
+    }
+
+    public void setPhotoID(UUID photoID) {
+        this.photoID = photoID;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+    /* getter setter end */
+    
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Comment comment)) return false;
+        return Objects.equals(uuid, comment.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(uuid);
+    }
 }
