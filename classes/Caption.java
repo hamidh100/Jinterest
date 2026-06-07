88a74ea2d13e60f764673a6990dbe72265675e9b
Hamid <hamidheydarian86@gmail.com>
2026-06-07 21:47:03 +0330

    caption added

---
+++ classes/Caption.java
@@ -0,0 +1,58 @@
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Caption {
+    private UUID photoID;
+    private String text;
+    private LocalDateTime time;
+    private final UUID uuid;
+    
+    public Caption(UUID photoID, String text) {
+        this.photoID = photoID;
+        this.text = text;
+        time = LocalDateTime.now();
+        uuid = UUID.randomUUID();
+    }
+    
+    /* getter setter begin */
+    public UUID getPhotoID() {
+        return photoID;
+    }
+
+    public void setPhotoID(UUID photoID) {
+        this.photoID = photoID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+    /* getter setter end */
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Caption caption)) return false;
+        return Objects.equals(uuid, caption.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(uuid);
+    }
+}
