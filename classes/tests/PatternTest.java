c967b2e99751f3353fd0d0b3108fb236210e50ee
Hamid <hamidheydarian86@gmail.com>
2026-06-09 23:50:57 +0330

    pattern tests improved

--- classes/tests/PatternTest.java
+++ classes/tests/PatternTest.java
@@ -37,7 +37,36 @@ public class PatternTest extends initTest {
     }
 
     @Test
+    public void emailTest(){
+        String identifier = "h@h.h";
+        assertFalse(identifier.matches(User.PHONENUMBER_PATTERN));
+        assertFalse(identifier.matches(User.USERNAME_PATTERN));
+        assertFalse(identifier.matches(User.USERNAME_DEFAULT_PATTERN));
+        assertFalse(identifier.matches(User.EMAIL_PATTERN));
+        assertFalse(identifier.matches(User.PASSWORD_PATTERN));
+
+        identifier = "h@h.ho";
+        assertTrue(identifier.matches(User.EMAIL_PATTERN));
+
+        identifier = "h09121112233.idk@some.thing";
+        assertFalse(identifier.matches(User.PHONENUMBER_PATTERN));
+        assertFalse(identifier.matches(User.USERNAME_PATTERN));
+        assertFalse(identifier.matches(User.USERNAME_DEFAULT_PATTERN));
+        assertTrue(identifier.matches(User.EMAIL_PATTERN));
+        assertFalse(identifier.matches(User.PASSWORD_PATTERN));
+    }
+
+    @Test
     public void passwordTest() {
-        
+        String password = "weakPASSWORD";
+        assertFalse(password.matches(User.PASSWORD_PATTERN));
+        password = "wPAS1!";
+        assertFalse(password.matches(User.PASSWORD_PATTERN));
+        password = "aaaaaaaaaaaaaaaaaaaa";
+        assertFalse(password.matches(User.PASSWORD_PATTERN));
+        password = "";
+        assertFalse(password.matches(User.PASSWORD_PATTERN));
+        password = "strongPASSWORD1";
+        assertTrue(password.matches(User.PASSWORD_PATTERN));
     }
 }
