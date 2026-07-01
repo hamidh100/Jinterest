fbfea360658630ab16175276923fc5a98ca81245
Hamid <hamidheydarian86@gmail.com>
2026-07-01 13:16:13 +0330

    caption.dart init

---
+++ frontend/lib/models/caption.dart
@@ -0,0 +1,13 @@
+class Caption {
+  final String uuid;
+  final String photoID;
+  final String text;
+  final DateTime time;
+
+  Caption({
+    required this.uuid,
+    required this.photoID,
+    required this.text,
+    required this.time,
+  });
+}
