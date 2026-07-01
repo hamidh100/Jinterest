0a5148a0068f621488a5e6e05075bc98217db8fc
Hamid <hamidheydarian86@gmail.com>
2026-07-01 13:10:32 +0330

    photo.dart init

---
+++ frontend/lib/models/photo.dart
@@ -0,0 +1,23 @@
+class Photo {
+  final String uuid;
+  final String ownerID;
+  final String path;
+  final String name;
+  final List<String> categoryList;
+  final String? captionText;
+  final DateTime photoAge;
+  final List<String> likeIDs;
+  final List<String> commentIDs;
+
+  Photo({
+    required this.uuid,
+    required this.ownerID,
+    required this.path,
+    required this.name,
+    this.categoryList = const [],
+    this.captionText,
+    required this.photoAge,
+    this.likeIDs = const [],
+    this.commentIDs = const [],
+  });
+}
