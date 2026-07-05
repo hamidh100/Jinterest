79b0bc6846aee3e1f036f6516a2b6739574e67ee
Hamid <hamidheydarian86@gmail.com>
2026-07-05 19:37:57 +0330

    album provider added

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/photo_provider.dart';
+import 'providers/album_provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => PhotoProvider()),
+        ChangeNotifierProvider(create: (_) => AlbumProvider()),
       ],
       child: MaterialApp(
         title: 'Jinterest',
