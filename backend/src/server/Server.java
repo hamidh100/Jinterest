1cf4157ced22000a7cf762d4bfe3e1f1e104e5c3
matin <mpourmanouchehri@gmail.com>
2026-07-15 15:34:16 +0330

    Multithreaded server class added

---
+++ backend/src/server/Server.java
@@ -0,0 +1,26 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final int port;
+    private final Router router;
+
+    public Server(int port) {
+        this.port = port;
+        this.router = new Router();
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Jinterest server listening on port " + port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                Thread thread = new Thread(new ClientHandler(clientSocket, router));
+                thread.start();
+            }
+        }
+    }
+}
