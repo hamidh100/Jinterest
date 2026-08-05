22c89cf685bc3f4c0b4fcfe7e4556abb74fd93cb
matin <mpourmanouchehri@gmail.com>
2026-08-05 18:36:29 +0330

    add username to comment.dart model

--- frontend/lib/models/comment.dart
+++ frontend/lib/models/comment.dart
@@ -2,6 +2,7 @@ class Comment {
   final String uuid;
   final String photoID;
   final String userID;
+  final String? username;
   final String text;
   final DateTime time;
 
@@ -9,6 +10,7 @@ class Comment {
     required this.uuid,
     required this.photoID,
     required this.userID,
+    this.username,
     required this.text,
     required this.time,
   });
