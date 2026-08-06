c89581159887b91ef01e4dc27e0060697799173f
matin <mpourmanouchehri@gmail.com>
2026-08-06 07:50:33 +0330

    purple orange palette

--- frontend/lib/theme/app_palette.dart
+++ frontend/lib/theme/app_palette.dart
@@ -4,8 +4,8 @@ class AppPalette {
   AppPalette._();
 
   // Dark-mode palette: change only these four colors to test a new palette.
-  static const background = Color(0xFF000000);
-  static const surface = Color(0xFF0B192C);
-  static const surfaceHighlight = Color(0xFF1E3E62);
-  static const accent = Color(0xFFFF6500);
+  static const background = Color(0xFF211832);
+  static const surface = Color(0xFF5C3E94);
+  static const surfaceHighlight = Color(0xFFB73A3A);
+  static const accent = Color(0xFFF25912);
 }
